fdb02a00935af83fea12b8675ba83fa4837678d4
Kinn-max <kienhien200418@gmail.com>
2025-03-03 18:07:21 +0700

    first commit

---
+++ backend/funiturestore/src/main/java/com/kinn/funiturestore/FuniturestoreApplication.java
@@ -0,0 +1,13 @@
+package com.kinn.funiturestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuniturestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuniturestoreApplication.class, args);
+	}
+
+}
---
+++ backend/funiturestore/src/test/java/com/kinn/funiturestore/FuniturestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinn.funiturestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuniturestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
