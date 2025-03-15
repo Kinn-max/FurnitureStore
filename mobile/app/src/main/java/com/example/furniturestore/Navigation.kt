0ec1ee9c15ddac095ce8b0b168fd0909a2b9ea7e
Kinn-max <kienhien200418@gmail.com>
2025-03-15 07:32:01 +0700

    upload bottomBar

--- mobile/app/src/main/java/com/example/furniturestore/Navigation.kt
+++ mobile/app/src/main/java/com/example/furniturestore/Navigation.kt
@@ -2,21 +2,34 @@ package com.example.furniturestore
 
 import androidx.navigation.compose.NavHost
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -29,6 +42,8 @@ sealed class Screen(val route:String){
     object Home:Screen("home")
     object Search:Screen("search")
     object Profile:Screen("profile")
+    object Wishlist:Screen("favorite")
+    object Cart:Screen("favorite")
     object Login:Screen("login")
     object Register:Screen("register")
 }
@@ -49,24 +64,74 @@ fun Navigation() {
 
     Scaffold(
         bottomBar = {
-            NavigationBar {
+            NavigationBar(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                    .background(Color.White),
+                tonalElevation = 8.dp
+            ) {
                 NavigationBarItem(
                     selected = navController.currentDestination?.route == Screen.Home.route,
                     onClick = { navController.navigate(Screen.Home.route) },
-                    label = { Text("Home") },
-                    icon = { Icon(Icons.Filled.Home, contentDescription = "Home Icon") }
+                    label = { Text("Home", color = Color.Black) },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = "Home",
+                            tint = Color.Black
+                        )
+                    }
                 )
                 NavigationBarItem(
-                    selected = navController.currentDestination?.route == Screen.Search.route,
-                    onClick = { navController.navigate(Screen.Search.route) },
-                    label = { Text("Search") },
-                    icon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") }
+                    selected = navController.currentDestination?.route == Screen.Wishlist.route,
+                    onClick = { navController.navigate(Screen.Wishlist.route) },
+                    label = { Text("Wishlist", color = Color.Gray) },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = "Wishlist",
+                            tint = Color.Gray
+                        )
+                    }
                 )
+                Box(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .background(Color(0xFFF6D56E), shape = CircleShape),
+                    contentAlignment = Alignment.Center
+                ) {
+                    IconButton (onClick = { navController.navigate(Screen.Search.route) }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Search",
+                            tint = Color.Black,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                }
                 NavigationBarItem(
                     selected = navController.currentDestination?.route == Screen.Profile.route,
-                    onClick = {navController.navigate(Screen.Profile.route) },
-                    label = { Text("Profile") },
-                    icon = { Icon(Icons.Filled.Person, contentDescription = "Profile Icon")  }
+                    onClick = { navController.navigate(Screen.Profile.route) },
+                    label = { Text("Profile", color = Color.Gray) },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Person,
+                            contentDescription = "Profile",
+                            tint = Color.Gray
+                        )
+                    }
+                )
+                NavigationBarItem(
+                    selected = navController.currentDestination?.route == Screen.Cart.route,
+                    onClick = { navController.navigate(Screen.Cart.route) },
+                    label = { Text("Cart", color = Color.Gray) },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.ShoppingCart,
+                            contentDescription = "Cart",
+                            tint = Color.Gray
+                        )
+                    }
                 )
             }
         }
